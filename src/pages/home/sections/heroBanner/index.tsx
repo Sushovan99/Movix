d519e192ee16c51a406881a7cc3c66a444611395
Sushovan Biswas <biswassushovan99@gmail.com>
2023-04-29 12:26:17 +0530

    Fix some scss issue in hero-banner

--- src/pages/home/sections/heroBanner/index.tsx
+++ src/pages/home/sections/heroBanner/index.tsx
@@ -5,26 +5,22 @@ import { useGetUpcomingMoviesQuery } from "@/store/apiSlices/getUpcomingMovies";
 import { useAppSelector } from "@/store/hooks";
 import Img from "@/components/LazyLoadImage";
 import ContentWrapper from "@/components/ContentWrapper";
-import { Result as Movie } from "@/service/models";
 
 const HeroBanner: React.FunctionComponent = () => {
-    const { data: movies, isSuccess } = useGetUpcomingMoviesQuery();
+    const { data: movies } = useGetUpcomingMoviesQuery();
     const Navigate = useNavigate();
     const [query, setQuery] = useState("");
-    const [randomMovie, setRandomMovie] = useState<Movie>();
     const { backdrop_sizes, base_url } = useAppSelector(
         (state) => state.dimensions
     );
 
+    const [backdropPath, setBackdropPath] = useState("");
+
     useEffect(() => {
-        if (isSuccess) {
-            setRandomMovie(
-                movies.results[
-                    Math.floor(Math.random() * movies.results.length)
-                ]
-            );
-        }
-    }, [backdrop_sizes.original, base_url, isSuccess, movies]);
+        const backdrop =
+            movies?.results[Math.floor(Math.random() * 20)].backdrop_path;
+        if (backdrop) setBackdropPath(backdrop);
+    }, [movies?.results]);
 
     const searchQueryHandler = (e: React.KeyboardEvent) => {
         if (e.key === "Enter" && query.length > 0) {
@@ -36,34 +32,29 @@ const HeroBanner: React.FunctionComponent = () => {
     };
 
     return (
-        <section className="hero-banner">
-            <div className="hero-banner__backdrop-img">
+        <section className="heroBanner">
+            <div className="backdrop-img">
                 <Img
                     src={
-                        randomMovie?.backdrop_path
-                            ? base_url +
-                              backdrop_sizes.original +
-                              randomMovie.backdrop_path
+                        backdropPath
+                            ? base_url + backdrop_sizes.original + backdropPath
                             : ""
                     }
-                    alt={randomMovie?.original_title}
+                    alt={backdropPath}
                 />
             </div>
 
-            <div className="wrapper-gradient"></div>
+            <div className="opacity-layer"></div>
 
             <ContentWrapper>
-                <div className="hero-banner__content">
-                    <div className="hero-banner__greeting">
-                        <h1 className="hero-banner__greeting__title">
-                            Welcome.
-                        </h1>
-                        <span className="hero-banner__greeting__subtitle">
-                            Thousands of movies, TV shows and people to
-                            discover. Explore now.
-                        </span>
-                    </div>
-                    <div className="hero-banner__search-grp">
+                <div className="heroBannerContent">
+                    <h1 className="title">Welcome.</h1>
+                    <span className="subTitle">
+                        Thousands of movies, TV shows and people to discover.
+                        Explore now.
+                    </span>
+
+                    <div className="searchInput">
                         <input
                             type="text"
                             onChange={(e) => setQuery(e.target.value)}
