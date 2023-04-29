7ab018f4d103777bb43bab44a541639e33dc205a
Sushovan Biswas <biswassushovan99@gmail.com>
2023-04-29 22:47:31 +0530

    Add Circular Rating component

---
+++ src/components/CircleRating/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import "./style.scss";
+
+interface Props {
+    rating: number;
+}
+
+const CircleRating: React.FunctionComponent<Props> = ({ rating }) => {
+    return (
+        <div className="circleRating">
+            <CircularProgressbar
+                value={rating}
+                maxValue={10}
+                text={`${rating}`}
+                styles={buildStyles({
+                    pathColor:
+                        rating < 5 ? "red" : rating < 7 ? "orange" : "green",
+                })}
+            />
+        </div>
+    );
+};
+
+export default CircleRating;
