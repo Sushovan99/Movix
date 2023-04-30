3ab39c45b36c38e265c883e9924af0b9df8409f6
Sushovan Biswas <biswassushovan99@gmail.com>
2023-04-30 12:33:43 +0530

    Add Genre component

---
+++ src/components/Genres/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useAppSelector } from "@/store/hooks";
+import "./style.scss";
+
+interface Props {
+    genreIDs: number[];
+}
+
+const Genres: React.FunctionComponent<Props> = ({ genreIDs }) => {
+    const { genre } = useAppSelector((state) => state.genres);
+
+    return (
+        <div className="genres">
+            {genreIDs.map((g) => (
+                <div className="genre" key={g}>
+                    {genre[g] || "no-genre"}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Genres;
